7b0125ae358fbad987213cd9d07e0f9c3c31a670
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-09 16:31:09 -0700

    simonSays completed.

--- simon_says/simonSays.js
+++ simon_says/simonSays.js
@@ -1,21 +1,26 @@
-function echo() {
-	
+function echo(simonSaid) {
+	return simonSaid;
 }
 
-function shout() {
-	
+function shout(simonSaid) {
+	return simonSaid.toUpperCase();
 }
 
-function repeat() {
-
+function repeat(simonSaid, count=2) {
+	let repeated = ''
+	for (let i = 0; i < count; i++) {
+		repeated += simonSaid;
+		if (i != count - 1) repeated += ' '
+	}
+	return repeated;
 }
 
-function pieceOfWord() {
-	
+function pieceOfWord(simonSaid, index) {
+	return simonSaid.slice(0, index);
 }
 
-function firstWord() {
-	
+function firstWord(simonSaid) {
+	return simonSaid.split(' ')[0];
 }
 
 function capitalize(word) {
@@ -23,8 +28,16 @@ function capitalize(word) {
 	// This function just capitalizes the word given to it, use in conjunction with titleCreator
 }
 
-function titleCreator() {
-	
+function titleCreator(simonSaid) {
+	let wordList = simonSaid.split(' ');
+	let title = '';
+	let littleWords = ['and', 'of', 'the', 'over'];
+	for (let i = 0; i < wordList.length; i++) {
+		let word = wordList[i];
+		littleWords.includes(word) && i != 0 ? (title += word) : (title += capitalize(word));
+		if (i !== wordList.length - 1) title += ' ';
+	}
+	return title;
 }
 
 module.exports = {
