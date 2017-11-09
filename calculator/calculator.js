8e534aa80eee471069257dbfabdf6aa68b9300d7
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-09 15:27:19 -0700

    Completed calculator.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	let sum = 0;
+	for (i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return Number(sum);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let product = 1;
+	for (i = 0; i < array.length; i++) {
+		product *= array[i];
+	}
+	return product;
 }
 
-function power() {
-	
+function power(a, b) {
+	return a**b;
 }
 
-function factorial() {
-	
+function factorial(number) {
+	result = 1;
+	for (i = 1; i <= number; i++) {
+		result *= i;
+	}
+	return result;
 }
 
 module.exports = {
