906138d7418255c716a5f430fbafb530d8be3a67
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-09 17:20:41 -0700

    Completed palindromes.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-var palindromes = function() {
+var palindromes = function(input) {
+  let array = input.split('');
+  let reversedString = array.reverse().toString();
 
-}
+  reversedString = reversedString.replace(/[.,!]/g, '')
+                                 .replace(/\s/g,"");
+  input = input.replace(/[.,!]/g, '')
+               .replace(/\s/g,"");
+
+  reversedString = reversedString.toLowerCase();
+  input = input.toLowerCase();
+  return reversedString === input;
+};
 
 module.exports = palindromes
