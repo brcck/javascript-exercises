ee2cc490d47597005d583b67787ddd7cb0888685
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-10 15:32:15 -0700

    Completed fibonacci.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-var fibonacci = function() {
-
+var fibonacci = function(number) {
+  if (number < 0) return "OOPS";
+  let sequence = [];
+  for (let i = 0; i < number; i++) {
+    if (i < 2) {
+      sequence[i] = 1;
+    } else {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+  }
+  return sequence.pop();
 }
 
 module.exports = fibonacci
